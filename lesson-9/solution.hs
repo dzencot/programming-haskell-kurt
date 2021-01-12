f3b868aed91c6248cbd2fbd02a6578e68422ac16
Ivan Gagarinov <dzencot@gmail.com>
2021-01-13 00:55:56 +0500

    finish lesson-9

--- lesson-9/solution.hs
+++ lesson-9/solution.hs
@@ -1,3 +1,5 @@
+import Data.Char
+
 myMap _ [] = []
 myMap func (x:xs) = func x : myMap func xs
 
@@ -12,8 +14,29 @@ remove test (x:xs) =
 
 myProduct = foldl (*) 1
 
+myFoldl f init [] = init
+myFoldl f init (x:xs) = myFoldl f newInit xs
+  where newInit = f init x
+
+myFoldr f init [] = init
+myFoldr f init (x:xs) = f x rightResult
+  where rightResult = myFoldr f init xs
+
+myElem pattern xs = length (filter (== pattern) xs) > 0
+
+isPalindrome text = correctText == reverse correctText
+  where correctText = map toLower (filter (/= ' ') text)
+
+harmonic count = foldr (+) 0 (map (1/) [2 .. count])
+
 main = do
   print (add3ToAll [1,2,3])
   print (mul3ByAll [1,2,3])
   print (remove even [1,2,3,4])
   print (myProduct [1,2,3,4])
+  print (myFoldl (+) 0 (take 4 [1..]))
+  print (myFoldr (-) 0 (take 5 [1..]))
+  print (myElem 1 [1,2,3])
+  print (myElem 4 [1,2,3])
+  print (isPalindrome "TEST tset")
+  print (harmonic 5)
